5df71cf98d1126a6c96556bf2d761e2b46c6e49a
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-06 11:40:00 +0200

    my first view

--- photonav/views.py
+++ photonav/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+
 
 # Create your views here.
+def gallery(request):
+    images = Image.objects.all()
+    categories = Category.objects.all()
+    locations = Locations.objects.all()
+    return render(request, 'index.html', {"images":images, "categories":categories, "locations":locations})
