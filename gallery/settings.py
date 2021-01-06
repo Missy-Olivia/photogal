e86900126936fa21d0aab89abc450c9d137dd40a
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-06 13:36:50 +0200

    added models

--- gallery/settings.py
+++ gallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'photonav',
     'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -76,11 +77,13 @@ WSGI_APPLICATION = 'gallery.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gallery',
+        'USER': 'missy',
+        'PASSWORD':'yeet',
+        'HOST': 'localhost',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -106,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Kigali'
 
 USE_I18N = True
 
--- photonav/views.py
+++ photonav/views.py
@@ -9,3 +9,24 @@ def gallery(request):
     categories = Category.objects.all()
     locations = Locations.objects.all()
     return render(request, 'index.html', {"images":images, "categories":categories, "locations":locations})
+
+def picture(request,category_name,image_id):
+    title = 'Image'
+    locations = Location.objects.all()
+    image_category = Image.objects.filter(image_category__name = category_name)
+    try:
+        image = Image.objects.get(id = image_id)
+    except DoesNotExist:
+        raise Http404()
+    return render(request,"picture.html",{'title':title,"image":image, "locations":locations, "image_category":image_category})
+
+def search_by_category(request):
+    title = 'Search'
+    if 'image_category' in request.GET and request.GET['image_category']:
+        search_term = request.GET.get('image_category')
+        found_results = Image.objects.filter(image_category__name=search_term)
+        message = f"{search_term}"
+        return render(request, 'search.html',{'title':title,'search_term':search_term,'images': found_results, 'message': message})
+    else:
+        message = 'You havent searched yet'
+        return render(request, 'search.html',{"message": message})
