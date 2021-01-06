4e6009be5dc5b44cf797e4282f686fc4584ae947
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-06 13:38:17 +0200

    added models

--- photonav/models.py
+++ photonav/models.py
@@ -3,3 +3,39 @@ import datetime as dt
 
 class Category(models.Model):
     name = models.CharField(max_length =50)
+    def save_category(self):
+        self.save()
+
+    def delete_category(self):
+        self.delete()
+
+    def update_category(self, update):
+        self.name = update
+        self.save()
+
+    @classmethod
+    def get_category_id(cls, id):
+        category = Category.objects.get(pk = id)
+        return category
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    name = models.CharField(max_length =50)
+
+    @classmethod
+    def tag_articles(cls):
+        tags = cls.objects.all()
+        return tags
+
+    def save_location(self):
+        self.save()
+
+    def delete_location(self):
+        self.delete()
+
+    def update_location(self, update):
+        self.name = update
+        self.save()
