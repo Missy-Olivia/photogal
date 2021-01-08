0d69ba1815fe84a747ce0e5e02747195a0b266ea
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-08 08:29:35 +0200

    updated nav and urls.py

--- photonav/urls.py
+++ photonav/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^$', views.gallery, name = 'gallery'),
+    url(r'^search/', views.search_by_category, name='search_by_category'),
+    url(r'^image/(?P<category_name>\w+)/(?P<image_id>\d+)',views.picture,name = 'picture'),
+    url(r'^location/(?P<image_location>\d+)', views.location_filter, name='location_filter')
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
