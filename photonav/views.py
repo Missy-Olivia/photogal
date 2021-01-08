4f70500408a80d475d2eca644e5dd1e56b9834cc
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-08 08:23:57 +0200

    updated views

--- photonav/views.py
+++ photonav/views.py
@@ -8,7 +8,7 @@ from .models import Image, Location, Category
 def gallery(request):
     images = Image.objects.all()
     categories = Category.objects.all()
-    locations = Locations.objects.all()
+    locations = Location.objects.all()
     return render(request, 'index.html', {"images":images, "categories":categories, "locations":locations})
 
 def picture(request,category_name,image_id):
