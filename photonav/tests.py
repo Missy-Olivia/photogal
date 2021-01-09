71b8dd36b20db6731e48b353165b52da56c21d47
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-09 22:36:16 +0200

    tested app

--- photonav/tests.py
+++ photonav/tests.py
@@ -30,3 +30,31 @@ class LocationTestCLass(TestCase):
         location.update_location('Glasgow')
         location = Location.get_location_id(self.locate.id)
         self.assertTrue(location.name == 'Glasgow')
+
+class CategoryTestClass(TestCase):
+
+    #setup method
+    def setUp(self):
+        self.categ = Category(name="Art")
+        self.categ.save_category()
+
+    #Testing Instances
+    def test_instance(self):
+        self.assertTrue(isinstance(self.categ, Category))
+
+    def test_save_method(self):
+        self.categ.save_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) > 0)
+
+    def test_delete_method(self):
+        self.categ.save_category()
+        self.categ.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
+
+    def test_update(self):
+        category = Category.get_category_id(self.categ.id)
+        category.update_category('Photography')
+        category = Category.get_category_id(self.categ.id)
+        self.assertTrue(category.name == 'Photography')
