36950ccb24fafa02d123c53c73185fc11499146d
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-09 22:29:26 +0200

    created 1st test

--- photonav/tests.py
+++ photonav/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from .models import Image, Category, Location
+import datetime as date
 
 # Create your tests here.
+class LocationTestCLass(TestCase):
+
+    #setup method
+    def setUp(self):
+        self.locate = Location(name="Africa")
+        self.locate.save_location()
+        
+    #Testing Instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.locate,Location))
+
