54f9ecd980c59ace3717b666ffe06c3efd8a5ffc
Ngabire Olivia Missy <ngamissia@gmail.com>
2021-01-09 22:33:28 +0200

    tested app

--- photonav/tests.py
+++ photonav/tests.py
@@ -9,8 +9,24 @@ class LocationTestCLass(TestCase):
     def setUp(self):
         self.locate = Location(name="Africa")
         self.locate.save_location()
-        
-    #Testing Instance
+
+    #Testing Instances
     def test_instance(self):
         self.assertTrue(isinstance(self.locate,Location))
 
+    def test_save_method(self):
+        self.locate.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+    def test_delete_method(self):
+        self.locate.save_location()
+        self.locate.delete_location()
+        location = Location.objects.all()
+        self.assertTrue(len(location) == 0)
+
+    def test_update(self):
+        location = Location.get_location_id(self.locate.id)
+        location.update_location('Glasgow')
+        location = Location.get_location_id(self.locate.id)
+        self.assertTrue(location.name == 'Glasgow')
